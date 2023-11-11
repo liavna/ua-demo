f164b3e56a0a7612e47072f0fea96e3ef5af9ee9
liavna <liavna@gmail.com>
2023-11-11 11:35:38 +0200

    Update get_alerts.py

--- alert/get_alerts.py
+++ alert/get_alerts.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.sensors import HttpSensor
+from airflow.operators.python_operator import PythonOperator
+import json
 
 default_args = {
     'owner': 'airflow',
@@ -15,36 +17,47 @@ default_args = {
 }
 
 dag = DAG(
-    'json_download_dag',
+    'https_json_dag',
     default_args=default_args,
-    description='A simple DAG to retrieve a JSON file from a URL',
-    schedule_interval=timedelta(days=1),  # Set the desired schedule interval
+    description='A simple DAG to fetch JSON data from an HTTPS site',
+    schedule_interval=timedelta(days=1),  # Set your desired schedule
 )
 
-# Sensor to wait for the URL to become available
-wait_for_url_sensor = HttpSensor(
-    task_id='wait_for_url_sensor',
-    http_conn_id='http_default',  # Set your HTTP connection ID
-    endpoint='/path/to/your/url',
-    timeout=600,  # Set an appropriate timeout value
-    poke_interval=60,  # Set an appropriate interval for poking
+# Use HttpSensor to wait until the site is available
+wait_for_site = HttpSensor(
+    task_id='wait_for_site',
+    method='HEAD',
+    http_conn_id='http_default',
+    endpoint='/path/to/your/site',
+    poke_interval=5,  # Check every 5 seconds
+    timeout=20,  # Timeout after 20 seconds
     dag=dag,
 )
 
-# Operator to download the JSON file
-download_json_operator = SimpleHttpOperator(
-    task_id='download_json_operator',
-    http_conn_id='http_default',  # Set your HTTP connection ID
-    endpoint='/path/to/your/jsonfile.json',
+# Use HttpOperator to get JSON data from the site
+get_json_data = SimpleHttpOperator(
+    task_id='get_json_data',
     method='GET',
-    headers={"Content-Type": "application/json"},
-    response_check=lambda response: True if response.status_code == 200 else False,
-    xcom_push=True,  # Push the response to XCom for potential downstream tasks
+    http_conn_id='http_default',
+    endpoint='/path/to/your/json/endpoint',
+    response_check=lambda response: True if response.json() else False,
     dag=dag,
 )
 
-# Set the task dependencies
-wait_for_url_sensor >> download_json_operator
+# Use PythonOperator to process the JSON data
+def process_json(**kwargs):
+    ti = kwargs['ti']
+    response = ti.xcom_pull(task_ids='get_json_data')
+    json_data = response.json()
+    # Add your processing logic here
+    print(json_data)
 
-if __name__ == "__main__":
-    dag.cli()
+process_json_task = PythonOperator(
+    task_id='process_json',
+    python_callable=process_json,
+    provide_context=True,
+    dag=dag,
+)
+
+# Define the task dependencies
+wait_for_site >> get_json_data >> process_json_task
