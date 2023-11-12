8c58c10bef5c758d05de7bcceb45521c70c21722
liavna@gmail.com <liavna@gmail.com>
2023-11-12 10:06:23 +0200

    dddd

--- alert/get_alerts.py
+++ alert/get_alerts.py
@@ -1,14 +1,16 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
-from airflow.operators.sensors import HttpSensor
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.sensors.http_sensor import HttpSensor
+from airflow.utils.dates import days_ago
 import json
-import requests
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -16,45 +18,42 @@ default_args = {
 dag = DAG(
     'json_file_from_url',
     default_args=default_args,
-    schedule_interval=timedelta(days=1),  # Adjust as needed
+    description='A DAG to read JSON file from URL',
+    schedule_interval=timedelta(days=1),
 )
 
 # Define the URL of the JSON file
-json_file_url = 'https://example.com/data.json'
+json_url = 'https://example.com/path/to/your/json/file.json'
 
-# Define a task to wait for the JSON file to be available
+# Use HttpSensor to wait until the file is available
 wait_for_file = HttpSensor(
     task_id='wait_for_file',
-    http_conn_id='http_default',  # Default connection ID
-    endpoint='',  # Leave it empty as we are using the full URL
-    request_params={},  # Additional request parameters if needed
-    mode='poke',  # Use poke mode to repeatedly check the file's availability
-    timeout=600,  # Timeout in seconds
-    poke_interval=60,  # Interval between pokes in seconds
+    method='HEAD',
+    http_conn_id='http_default',  # You may need to define an HTTP connection in Airflow
+    endpoint=json_url,
+    poke_interval=5,
+    timeout=20,
     dag=dag,
 )
 
-# Define a task to download the JSON file
-download_file = SimpleHttpOperator(
-    task_id='download_file',
-    http_conn_id='http_default',  # Default connection ID
+# Use SimpleHttpOperator to fetch the JSON file
+fetch_file = SimpleHttpOperator(
+    task_id='fetch_file',
     method='GET',
-    endpoint=json_file_url,  # Use the full URL as the endpoint
-    headers={},  # Additional headers if needed
-    response_filter=lambda response: response.json(),  # Parse the JSON response
-    xcom_push=True,  # Push the downloaded data to XCom for the next task
+    http_conn_id='http_default',
+    endpoint=json_url,
+    xcom_push=True,
     dag=dag,
 )
 
-# Define a task to process the JSON data
+# Define a Python function to process the JSON data
 def process_json_data(**kwargs):
     ti = kwargs['ti']
-    json_data = ti.xcom_pull(task_ids='download_file')
-    
+    json_data = ti.xcom_pull(task_ids='fetch_file')
     # Process the JSON data as needed
-    # For example, print the data
     print(json_data)
 
+# Use a PythonOperator to process the JSON data
 process_data = PythonOperator(
     task_id='process_data',
     python_callable=process_json_data,
@@ -62,5 +61,5 @@ process_data = PythonOperator(
     dag=dag,
 )
 
-# Set up the task dependencies
-wait_for_file >> download_file >> process_data
+# Set up task dependencies
+wait_for_file >> fetch_file >> process_data
