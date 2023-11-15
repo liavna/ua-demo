ff7b6e0bdc423b823671e18a2c9096a92723e4f8
liavna <liavna@gmail.com>
2023-11-15 10:52:46 +0200

    Update get_alerts.py

--- alert/get_alerts.py
+++ alert/get_alerts.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
+import json
+import requests
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-import requests
-import json
 
-# Define the default_args dictionary to specify the default parameters of the DAG.
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -13,11 +12,23 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Define the DAG with the specified default_args.
+def download_and_save_json(**kwargs):
+    url = "https://www.oref.org.il/WarningMessages/History/AlertsHistory.json"
+    save_path = "/usr/local/airflow/AlertsHistory.json"
+
+    response = requests.get(url)
+    data = response.json()
+
+    with open(save_path, 'w') as json_file:
+        json.dump(data, json_file)
+
+    print(f"Downloaded and saved JSON data to {save_path}")
+
 dag = DAG(
-    'read_json_from_url',
+    'json_download_dag',
     default_args=default_args,
-    schedule_interval=timedelta(days=1),  # Adjust the schedule_interval as needed.
+    description='DAG to download JSON from URL and save locally',
+    schedule_interval='*/5 * * * *',  # Run every 5 minutes
     access_control={
 		'role_liav': {
 			'can_read',
@@ -33,36 +44,15 @@ dag = DAG(
 
 )
 
-# Define the function that will be executed by the PythonOperator.
-def fetch_and_process_json(**kwargs):
-    url = "https://www.oref.org.il/WarningMessages/History/AlertsHistory.json"
-    
-    try:
-        # Fetch JSON data from the URL.
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad responses.
-
-        # Parse the JSON data.
-        json_data = response.json()
-
-        # Do further processing with the JSON data as needed.
-        # For example, print the content of the JSON data.
-        print(json.dumps(json_data, indent=2))
-
-    except Exception as e:
-        # Handle exceptions (e.g., connection errors, JSON parsing errors).
-        print(f"Error fetching or processing JSON: {str(e)}")
-
-# Define the PythonOperator to execute the fetch_and_process_json function.
-fetch_json_task = PythonOperator(
-    task_id='fetch_json',
-    python_callable=fetch_and_process_json,
+download_task = PythonOperator(
+    task_id='download_json_task',
+    python_callable=download_and_save_json,
     provide_context=True,
     dag=dag,
 )
 
-# Set the task dependencies.
-fetch_json_task
+# Set task dependencies if you have more tasks in the DAG
+# task2.set_upstream(task1)
 
 if __name__ == "__main__":
     dag.cli()
