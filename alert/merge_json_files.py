6cec0a8e83c3769df660fb7281001766ba857c0f
liavna <liavna@gmail.com>
2023-11-25 22:16:00 +0200

    Update merge_json_files.py

--- alert/merge_json_files.py
+++ alert/merge_json_files.py
@@ -9,11 +9,12 @@ def run_spark_job():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
+    'start_date': days_ago(1),
+    'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'max_active_runs': 1,
+    'retries': 0
 }
 
 dag = DAG(
